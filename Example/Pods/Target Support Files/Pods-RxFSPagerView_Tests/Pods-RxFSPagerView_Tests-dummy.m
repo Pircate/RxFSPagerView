94f33e96e5b5bfa8c473c9a462ebf61303cc1ddf
Pircate <gao497868860@163.com>
2018-07-17 19:12:36 +0800

    INIT

---
+++ Example/Pods/Target Support Files/Pods-RxFSPagerView_Tests/Pods-RxFSPagerView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxFSPagerView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_RxFSPagerView_Tests
+@end
---
+++ Example/Pods/Target Support Files/RxFSPagerView/RxFSPagerView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RxFSPagerView : NSObject
+@end
+@implementation PodsDummy_RxFSPagerView
+@end
