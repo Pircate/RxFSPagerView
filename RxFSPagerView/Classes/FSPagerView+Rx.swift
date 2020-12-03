9737fb54cd1c476f926c523b1559393742395b42
Pircate <gao497868860@gmail.com>
2020-12-03 22:28:56 +0800

    style: 格式化代码

--- RxFSPagerView/Classes/FSPagerView+Rx.swift
+++ RxFSPagerView/Classes/FSPagerView+Rx.swift
@@ -10,36 +10,52 @@ import RxCocoa
 
 public extension Reactive where Base: FSPagerView {
     
-    func items<S: Sequence, Cell: FSPagerViewCell, O : ObservableType>
-        (cellIdentifier: String, cellType: Cell.Type = Cell.self)
-        -> (_ source: O)
-        -> (_ configureCell: @escaping (Int, S.Iterator.Element, Cell) -> Void)
-        -> Disposable where O.Element == S {
-            base.collectionView.dataSource = nil
-            return { source in
-                let source = source.map({ sequence -> S in
-                    let items = Array(sequence)
-                    self.base.numberOfSections = items.count
-                    let max = self.base.isInfinite && (items.count > 1 || !self.base.removesInfiniteLoopForSingleItem) ? Int8.max : 1
-                    var actualItems: [S.Element] = []
-                    (0..<max).lazy.forEach({ _ in actualItems += items })
-                    self.base.numberOfItems = actualItems.count
-                    return actualItems as! S
-                })
-                return self.base.collectionView.rx.items(cellIdentifier: cellIdentifier, cellType: cellType)(source)
+    typealias ConfigureCell<S: Sequence, Cell> = (Int, S.Iterator.Element, Cell) -> Void
+    
+    func items<S: Sequence, Cell: FSPagerViewCell, O: ObservableType>(
+        cellIdentifier: String,
+        cellType: Cell.Type = Cell.self
+    ) -> (_ source: O) -> (_ configureCell: @escaping ConfigureCell<S, Cell>) -> Disposable
+    where O.Element == S {
+        base.collectionView.dataSource = nil
+        return { source in
+            let source = source.map { sequence -> S in
+                let items = Array(sequence)
+                self.base.numberOfSections = items.count
+                
+                let shouldLoop = items.count > 1 || !self.base.removesInfiniteLoopForSingleItem
+                let max = self.base.isInfinite && shouldLoop ? Int8.max : 1
+                let actualItems: [S.Element] = (0..<max).lazy.reduce([]) { result, _ in
+                    result + items
+                }
+                self.base.numberOfItems = actualItems.count
+                
+                return actualItems as! S
             }
+            
+            return self.base.collectionView.rx.items(
+                cellIdentifier: cellIdentifier,
+                cellType: cellType
+            )(source)
+        }
     }
 }
 
 public extension Reactive where Base: FSPagerView {
     
     var itemSelected: ControlEvent<Int> {
-        let source = base.collectionView.rx.itemSelected.map { $0.item % self.base.numberOfSections }
+        let source = base.collectionView.rx.itemSelected.map {
+            $0.item % self.base.numberOfSections
+        }
+        
         return ControlEvent(events: source)
     }
     
     var itemDeselected: ControlEvent<Int> {
-        let source = base.collectionView.rx.itemDeselected.map { $0.item % self.base.numberOfSections }
+        let source = base.collectionView.rx.itemDeselected.map {
+            $0.item % self.base.numberOfSections
+        }
+        
         return ControlEvent(events: source)
     }
     
@@ -48,15 +64,17 @@ public extension Reactive where Base: FSPagerView {
     }
     
     var itemScrolled: ControlEvent<Int> {
-        let source = base.collectionView.rx.didScroll.flatMap({ _ -> Observable<Int> in
-            guard self.base.numberOfSections > 0 else { return Observable.never() }
+        let source = base.collectionView.rx.didScroll.flatMap { _ -> Observable<Int> in
+            guard self.base.numberOfSections > 0 else { return .never() }
+            
             let currentIndex = lround(Double(self.base.scrollOffset)) % self.base.numberOfSections
-            if currentIndex != self.base.currentIndex {
-                self.base.currentIndex = currentIndex
-                return Observable.just(currentIndex)
-            }
-            return Observable.never()
-        })
+            
+            guard currentIndex != self.base.currentIndex else { return .never() }
+            
+            self.base.currentIndex = currentIndex
+            return Observable.just(currentIndex)
+        }
+        
         return ControlEvent(events: source)
     }
 }
@@ -65,14 +83,10 @@ public extension Reactive where Base: FSPagerView {
     
     func deselectItem(animated: Bool) -> Binder<Int> {
         return Binder(base) { this, item in
-            this.collectionView.deselectItem(at: IndexPath(item: item, section: 0), animated: animated)
+            this.collectionView.deselectItem(
+                at: IndexPath(item: item, section: 0),
+                animated: animated
+            )
         }
     }
-}
-
-fileprivate func castOrThrow<T>(_ resultType: T.Type, _ object: Any) throws -> T {
-    guard let returnValue = object as? T else {
-        throw RxCocoaError.castingError(object: object, targetType: resultType)
-    }
-    return returnValue
 }
